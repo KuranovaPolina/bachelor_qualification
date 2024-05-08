f7158b676938bf6d11ee2f5ef0e9ba39cce434a2
Polina <kuranovapolina@gmail.com>
2024-05-09 00:10:42 +0300

    echo second thread

--- src/laptop/main.cpp
+++ src/laptop/main.cpp
@@ -1,11 +1,40 @@
 /* main.cpp for laptop */
 
 #include <iostream>
+#include <thread>
+#include <unistd.h>
 
 #include "stream.h"
 
+int processManage()
+{
+    int x = 0;
+    while (1)
+    {
+
+        /* Incorrect string value */
+        cout << "menu" << endl;
+
+        cin >> x;
+
+        cout << x << endl;
+
+        // usleep(1000000);
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    std::thread manage_thread([]()
+    {
+        if (processManage() == -1)
+        {
+            return -1;
+        }
+
+        return 0;
+    });
+
     Stream stream = Stream("params.json");
 
     if (stream.isOpened())
@@ -20,6 +49,8 @@ int main(int argc, char *argv[])
 
         return -1;
     }
+    
+    manage_thread.join();
 
     return 0;
 }
