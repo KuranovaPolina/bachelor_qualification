b46a463d061e3f7f9568b4628446c6f3c605321f
Kuranova Polina <kuranovapolina@gmail.com>
2024-05-09 00:10:02 +0300

    echo second thread

--- src/jetson/main.cpp
+++ src/jetson/main.cpp
@@ -1,12 +1,37 @@
 /* main.cpp for Jetson Nano program */
 
 #include <iostream>
+#include <thread>
 // #include <opencv2/opencv.hpp>
 
 #include "stream.h"
 
+int processManage()
+{
+    int x = 0;
+
+    while(1)
+    {
+        cout << "menu" << endl;
+
+        cin >> x;
+
+        cout << x << endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    std::thread manage_thread([]()
+    {
+        if (processManage() == -1)
+        {
+            return -1;
+        } 
+        
+        return 0;
+    });
+
     Stream stream = Stream("params.json");
 
     if (stream.isOpened())
@@ -21,6 +46,8 @@ int main(int argc, char *argv[])
 
         return -1;
     }
+
+    manage_thread.join();
 
     return 0;
 }
